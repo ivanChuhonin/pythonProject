ac4a7fd19474550e722609cd53c241aabd72d6c8
ivanChuhonin <ivachuhonin@niuitmo.ru>
2021-09-21 00:31:52 +0300

    init done

--- main.py
+++ main.py
@@ -1,16 +1,84 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Задача №1 вычислить сумму двух заданных чисел
+def summ():
+    a = int(input('Введите целое число : '))
+    b = int(input('Введите ещё одно целое число : '))
+    print(a+b)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm !')
+# Задача №2 требуется вычислить a+b^2
+def doublesumm():
+    a = int(input('Введите целое число : '))
+    b = int(input('Введите ещё одно целое число : '))
+    print(a+b**2)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Задача №3 пользуясь файлами, решить задачу a+b
+def filesumm():
+    f = open('input3.txt', 'r')
+    line = f.readline()
+    a,b = [int (x) for x in line.split()]
+    f.close()
+    f = open('output3.txt', 'w')
+    f.write(str (a+b))
+    f.close()
+
+
+# Задача №4 пользуясь файлами, решить задачу a+b^2
+def filedoublesumm():
+    f = open('input4.txt', 'r')
+    line = f.readline()
+    a,b = [int (x) for x in line.split()]
+    f.close()
+    f = open('output4.txt', 'w')
+    f.write(str (a+b**2))
+    f.close()
+
+
+# функция к заданию 2 вычисляет число Фиббоначи n
+def Fibo(n):
+    if n<0:
+        return -1
+    if n>45:
+        return -1
+    if n==0:
+        return 0
+    if n==1 or n==2:
+        return 1
+    firstf = 1
+    secondf = 1
+    for i in range(n-2):
+        firstf,secondf = secondf, firstf+secondf
+    return secondf
+# Задание 2 пользуясь файлами, находит число Фиббоначи n
+def Fib():
+    f = open("input2.txt")
+    a = int(f.readline())
+    f.close()
+    f = open("output2.txt","w")
+    f.write(str(Fibo(a)))
+    f.close()
+
+
+# функция к заданию 2.2 определение последней цифры числа Фибоначчи
+def Fibocheck(n):
+    f=Fibo(n)
+    return f%10
+# Задание 2.2 пользуясь файлами, определение последней цифры числа Фибоначчи
+def Fib2():
+    f = open("input.txt")
+    a = int(f.readline())
+    f.close()
+    f = open("output.txt","w")
+    f.write(str(Fibocheck(a)))
+    f.close()
+
+
+import time
+t_start = time.perf_counter()
+#summ()
+doublesumm()
+#filesumm()
+#filedoublesumm()
+#Fib()
+#Fib2()
+print("Время работы: %s секунд " % (time.perf_counter() - t_start))
